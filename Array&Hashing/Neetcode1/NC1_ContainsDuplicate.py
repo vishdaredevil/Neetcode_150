0d7e583c25eb5cde291507555c7e49c43e7db019
Taniya Vishnoi <vishnoitaniya17@gmail.com>
2025-09-02 02:19:51 +0530

    question 1 nd 2

---
+++ Array&Hashing/Neetcode1/NC1_ContainsDuplicate.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset=set()
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)
+        return False
